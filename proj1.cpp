ded046e6260199b8feaba9aa42c3c9b3b84febdb
Ryan Stevenson <steven_r1@denison.edu>
2018-03-01 10:37:20 -0500

    Second Addition of A couple of project files

---
+++ proj1.cpp
@@ -0,0 +1,41 @@
+//=======================================
+//Josh Blaz & Ryan Stevenson
+//1/1/16
+//proj1.cpp
+//Project 1: Zombiquarium. Find the most efficient
+//number of zombies to reach 5000 sun points as quickly as possible.
+//======================================
+
+#include <iostream>
+using namespace std;
+
+
+//======================================
+//Main
+//======================================
+
+int main ( )
+{
+	int sun = 100;
+	int zombies = 0;
+	int days = 1;
+	int maxZombie;
+	
+	
+	cout << " What would you like the max zombie count to be?: ";
+	cin >> maxZombie;
+	
+	int array[maxZombie];
+	
+	
+	while ( sun < 5000)
+	{
+		for ( 
+		
+		
+		cout << "Day " << days <<    
+		
+		
+	}
+	return 0
+}
